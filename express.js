08e729587d2d0cf26e181201385c569144ec5bf5
maxlichutin <maxlichutin@gmail.com>
2023-11-17 16:42:02 -0700

    Added SSL suppport

---
+++ express.js
@@ -0,0 +1,60 @@
+import express from "express";
+const app = express();
+const router = express.Router();
+import cors from "cors";
+app.use(cors());
+
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import { generateAccessToken } from "./jwt.js";
+import { broadcast } from "./wss.js";
+
+
+async function startExpress() {
+  // DEVICE STATUS CHANGE POST FROM HUB
+  router.post("/device-status", cors(), function (req, res) {
+    let query = req.query || "";
+    let headers = req.headers || {};
+    let body = req.body || {};
+    res.status(400).send("ok");
+
+    let data = {
+      messageType: "hubitatUpdate",
+      data: body,
+    };
+    broadcast(data);
+  });
+
+  // GET ACCESS
+  router.post("/get-access", cors(), function (req, res) {
+    //   let query = req.query || "";
+    let headers = req.headers || {};
+    let body = req.body || {};
+
+    if (process.env.PIN === body.pin) {
+      let result = {
+        status: "success",
+        message: "Access granted successfully.",
+        jwt: generateAccessToken(headers.origin),
+      };
+      res.status(200).send(result);
+    } else {
+      let result = {
+        status: "error",
+        message: "Invalid PIN. Please provide a valid PIN.",
+      };
+      res.status(400).send(result);
+    }
+  });
+
+  // Add JSON support for POST requests (req.body, etc)
+  app.use(express.json());
+  // Add the router
+  app.use("/", router);
+  // Listen
+  app.listen(process.env.HTTP_PORT || 8000);
+  console.log(`Running at Port ${process.env.HTTP_PORT || 8000}`);
+}
+
+export { startExpress as startExpress };
